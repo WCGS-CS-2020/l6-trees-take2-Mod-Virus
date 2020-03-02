e067883603470cf200c478d9f61957d4ef3b8028
Tin-Chi Wong <tinchiivanwong@yahoo.co.uk>
2020-03-02 20:48:56 +0000

    Copied Pasted Template
    Added the template code in

--- Binary Tree Program/Binary Tree Program/Program.cs
+++ Binary Tree Program/Binary Tree Program/Program.cs
@@ -2,11 +2,54 @@
 
 namespace Binary_Tree_Program
 {
+
+    /*
+     * Tasks:
+     * 1) Complete the implementation of the Node methods
+     * 2) Print out the tree using the different tree traversal metods
+     * 3) Test findNote() and deleteNode()
+     *
+     *
+     */
+    class Node
+    {
+        // Attributes
+        private Node left;
+        private Node right;
+        private string item;
+
+        //Methods
+        public Node(string item) { }
+        public void addNode(string item) { }
+        public Boolean findNode(string item) { return true; }
+        public Boolean deleteNote(string item) { return true; }
+        void printTree() { }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Node root = null;
+
+            string[] months = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+
+
+            // process all the nodes on the array
+            //
+            foreach (var mon in months)
+            {
+                if (root == null)
+                    root = new Node(mon);
+                else
+                    root.addNode(mon);
+            }
+
+            // print out the tree using different traversal methods
+            //
+
+            // Test the findNote() and deleteNode()
         }
     }
 }
+
